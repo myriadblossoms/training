fd4ecf97b5f2fe5959218c60330e963dab5fe2c2
홍명기 <myriadblossoms@naver.com>
2023-12-07 17:09:34 +0900

    .

--- Jstraining/tab.js
+++ Jstraining/tab.js
@@ -36,13 +36,14 @@ let btn = $('.tab-button')
 let content = $('.tab-content')
 
 let car = ['소나타',50000,'white'];
-
+let car2 = {name:'소나타', price : 50000, clor : 'white'}
 // for(let i=0; i<btn.length; i++){
 //     btn.eq(i).on('click',function(){
 //         탭열기(i);
 //     })
 // }
-
+document.querySelector('.carname').innerHTML = car2.name;
+document.querySelector('.carprice').innerHTML = car2.price;
 $('.list').click(function(e){
     탭열기(e.target.dataset.id)
 })
@@ -56,3 +57,55 @@ function 탭열기(숫자){
 // document.querySelector('.tab-button').addEventListener('click',function(){
 //     document.querySelector('.tab-button').classList.remove('orange')
 // })
+$('.form-select').eq(0).on('input',function(e){
+    let value =e.currentTarget.value;
+    let sizeJean = [28, 30, 32]
+    let sizeShirt =[95,100,105]
+
+    if( value == '셔츠'){
+        $('.form-select').eq(1).removeClass('form-hide');
+    }else{
+        $('.form-select').eq(1).addClass('form-hide');
+    }
+
+    if( value == '바지'){
+        $('.form-select').eq(1).removeClass('form-hide');
+        $('.form-select').eq(1).html('');
+        sizeJean.forEach(function(){
+
+        })
+     }
+     
+    })
+
+    var 출석부 = ['흥민', '영희', '철수', '재석'];
+
+    function 이름찾기(e){
+      if ( 출석부[2] == e ){
+        // console.log('있어요')
+      }else{
+        // console.log('없어요')
+      }
+    }
+    이름찾기('철수');
+
+    for( k = 2; k <10; k++){
+        for( i = 1; i<10; i++){
+            // console.log(k * i);
+        }
+    }
+
+    function 시험점수(a,b){
+        var result= 0;
+        for(i=0;i < a.length; i++){
+            result = result + a[i]
+        }
+        console.log(result)
+        
+        if(result/a.length < b){
+            console.log('점수업')
+        }else{
+            console.log('재수강')
+        }
+    }
+    시험점수([2,3,4],10)
