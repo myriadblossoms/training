0ca7d9f1294770644bc7f6b88c1373fcbe531e2b
홍명기 <myriadblossoms@naver.com>
2023-12-13 18:09:57 +0900

    .

--- Nodetraining/server.js
+++ Nodetraining/server.js
@@ -3,6 +3,8 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 app.set('view engine','ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 const { MongoClient } = require('mongodb')
 
@@ -39,6 +41,32 @@ app.get('/about', (요청, 응답) => {
     
   app.get('/list', async(요청, 응답) => {
     let result = await db.collection('post').find().toArray()
-    console.log(result[0].title)
-    응답.send('쇼핑페이지입니다')
+    // console.log(result[0].title)
+
+    응답.render('list.ejs',{ 글목록 : result})
+  }) 
+
+  app.get('/time', (요청, 응답) => {
+
+    응답.render('time.ejs', {시간:new Date()})
+  }) 
+
+  app.get('/write', (요청, 응답) => {
+
+    응답.render('write.ejs')
+  }) 
+
+  app.post('/add', async(요청, 응답) => {
+    console.log(요청.body)
+    try{
+      if(요청.body.title == ''){
+        응답.send('제목입력해주세요')
+      }else{
+        await db.collection('post').insertOne({title: 요청.body.title, content : 요청.body.content})
+        응답.redirect('/list')
+      }
+    }catch(e){
+      console.log(e)
+      응답.status(500).send('서버에러')
+    }
   }) 
